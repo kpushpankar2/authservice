aca6de2138deea703981b9dc7b44e4dbe56d5fcf
Pushpankar Singh <kpushpankar2@gmail.com>
2024-07-03 01:40:55 +0530

    test husky 3

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 function login(username: string): string {
-    return username;
+    // console.log(username);
+
+    const user = {
+        name: "Pushpankar",
+    };
+    const name = user["name"];
+    return username + name;
 }
 
 login("Pushpankar Singh");
