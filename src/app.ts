87fa4ff88c54b1fd23b068ae34e70adf3cc8dc32
Pushpankar Singh <kpushpankar2@gmail.com>
2024-07-07 03:18:18 +0530

    Implement Error handling using http-error

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,30 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import logger from "./config/logger";
+import { HttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     res.send("Welcone to Authentication Service");
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+
+                msg: err.message,
+
+                path: "",
+
+                location: "",
+            },
+        ],
+    });
 });
 
 export default app;
