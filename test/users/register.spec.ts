a52509a3afeb6f31c81614fe0e0996e4d9d8662a
Pushpankar Singh <kpushpankar2@gmail.com>
2024-10-07 13:16:13 +0530

    add validating field for email

--- test/users/register.spec.ts
+++ test/users/register.spec.ts
@@ -198,5 +198,33 @@ describe("POST /auth/register", () => {
         });
     });
 
-    describe("Fields are missing", () => {});
+    describe("Fields are missing", () => {
+        it("should return 400 status code if email field is missing", async () => {
+            //Arrange
+
+            const userData = {
+                firstName: "Pushpankar",
+
+                lastName: "Singh",
+
+                email: "",
+
+                password: "secret",
+            };
+
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+
+            //Asert
+
+            expect(response.statusCode).toBe(400);
+
+            const userRepository = connection.getRepository(User);
+
+            const users = await userRepository.find();
+
+            expect(users).toHaveLength(0);
+        });
+    });
 });
