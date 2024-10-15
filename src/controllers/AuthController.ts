0830790add218e58228c40625962d5b301d871a4
Pushpankar Singh <kpushpankar2@gmail.com>
2024-10-15 18:22:35 +0530

    refactor Tokens

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -4,8 +4,6 @@ import { ResgisterUserRequest } from "../types";
 import { UserService } from "../services/UserService";
 import { Logger } from "winston";
 import { validationResult } from "express-validator";
-import { AppDataSource } from "../config/data-source";
-import { RefreshToken } from "../entity/RefreshToken";
 import { TokenService } from "../services/TokenService";
 
 export class AuthController {
@@ -52,15 +50,18 @@ export class AuthController {
 
             const accessToken = this.tokenService.generateAccessToken(payload);
 
-            const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
-            const refreshTokenRepository =
-                AppDataSource.getRepository(RefreshToken);
+            const newRefreshToken =
+                await this.tokenService.persistRefreshToken(user);
 
-            const newRefreshToken = await refreshTokenRepository.save({
-                user: user,
+            // const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
+            // const refreshTokenRepository =
+            //     AppDataSource.getRepository(RefreshToken);
 
-                expiresAt: new Date(Date.now() + MS_IN_YEAR),
-            });
+            // const newRefreshToken = await refreshTokenRepository.save({
+            //     user: user,
+
+            //     expiresAt: new Date(Date.now() + MS_IN_YEAR),
+            // });
 
             // const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
             //     algorithm: "HS256",
