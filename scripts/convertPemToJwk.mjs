27195ec5cd3f5af557c6df39dd06c5399f260eb7
Pushpankar Singh <kpushpankar2@gmail.com>
2024-12-15 00:34:02 +0530

    setup coverage

--- scripts/convertPemToJwk.mjs
+++ scripts/convertPemToJwk.mjs
@@ -2,12 +2,9 @@ import fs from "fs";
 
 import rsaPemToJwk from "rsa-pem-to-jwk";
 
-
-const privateKey = fs.readFileSync('./certs/private.pem');
-
+const privateKey = fs.readFileSync("./certs/private.pem");
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unused-vars
-const jwk = rsaPemToJwk(privateKey, {use:'sig'}, "public")
+const jwk = rsaPemToJwk(privateKey, { use: "sig" }, "public");
 
-
-// console.log(JSON.stringify(jwk)); 
+// console.log(JSON.stringify(jwk));
--- src/migration/1734198985342-add_refresh_token_casde.ts
+++ src/migration/1734198985342-add_refresh_token_casde.ts
@@ -22,6 +22,5 @@ export class AddRefreshtokenCascade1699475145577 implements MigrationInterface {
     }
 }
 
-
 // npm run migration:create -- src/migration/add_refresh_token_casde  for creating empty migration
 // To Run Migration  npm run migration:run -- -d src/config/data-source.ts
